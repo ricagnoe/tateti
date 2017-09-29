ba4e544b12c6ca784226431c78752b3d306d1efb
vagrant <vagrant@kleer-csd-box>
2017-09-29 14:47:24 -0300

    tateti dentro de partida

--- lib/partida.rb
+++ lib/partida.rb
@@ -2,32 +2,34 @@ require "./lib/logica.rb"
 
 class Partida
 	def initialize
-		@@score_O = 0
-		@@score_X = 0
-		@@game = Tateti.new
+		@score_O = 0
+		@score_X = 0
+		@game = Tateti.new
 	end
-
+	def game
+		return @game
+	end
 	def score jugador
 		if jugador == "X"
-			return @@score_X
+			return @score_X
 		else
-			return @@score_O
+			return @score_O
 		end
 	end
 
 	def gano jugador
 		if jugador == "X"
-			@@score_X +=1
-			return @@score_X
+			@score_X +=1
+			return @score_X
 		else
-			@@score_O +=1
-			return @@score_O
+			@score_O +=1
+			return @score_O
 		end
 	end
 
 	def reset
-		@@score_X = 0
-		@@score_O = 0
+		@score_X = 0
+		@score_O = 0
 	end
 
 end
