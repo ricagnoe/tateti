ef46f2401f9b15d28930194a4da0a71a190d3b16
vagrant <vagrant@kleer-csd-box>
2017-09-29 12:17:46 -0300

    ganador 2

--- app.rb
+++ app.rb
@@ -3,15 +3,6 @@ require './lib/logica.rb'
 
 get '/' do
 		@@tateti = Tateti.new
-		@@td_1_1 = "<a href='/O/1_1' id='O_1_1'>[ ]</a>"
-		@@td_1_2 = "<a href='/O/1_2' id='O_1_2'>[ ]</a>"
-		@@td_1_3 = "<a href='/O/1_3' id='O_1_3'>[ ]</a>"
-		@@td_2_1 = "<a href='/O/2_1' id='O_2_1'>[ ]</a>"
-		@@td_2_2 = "<a href='/O/2_2' id='O_2_2'>[ ]</a>"
-		@@td_2_3 = "<a href='/O/2_3' id='O_2_3'>[ ]</a>"
-		@@td_3_1 = "<a href='/O/3_1' id='O_3_1'>[ ]</a>"
-		@@td_3_2 = "<a href='/O/3_2' id='O_3_2'>[ ]</a>"
-		@@td_3_3 = "<a href='/O/3_3' id='O_3_3'>[ ]</a>"
     erb :tateti_pablo
 end
 
